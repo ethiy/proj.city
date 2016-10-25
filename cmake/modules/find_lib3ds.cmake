c0838274bd012f1ed8010f9e2b883f6d9050c42b
Ethiy <oussama.ennafii@ign.fr>
2016-10-25 11:08:42 +0200

    cmake finds lib3ds

--- cmake/modules/find_lib3ds.cmake
+++ cmake/modules/find_lib3ds.cmake
@@ -3,7 +3,6 @@
 # LIB3DS_INCLUDE_DIRS - where the lib3ds directory containing the headers can be
 #                      found
 # LIB3DS_LIBRARIES     - full path to the lib3ds library
-# LIB3DS_FOUND       - true if lib3ds was found
 
 find_path(LIB3DS_INCLUDE_DIRS lib3ds/file.h
     /usr/include
@@ -23,13 +22,3 @@ if(LIB3DS_LIBRARIES)
 else(LIB3DS_LIBRARIES)
     message(STATUS "Could NOT find lib3ds library.")
 endif(LIB3DS_LIBRARIES)
-
-
-if(LIB3DS_INCLUDE_DIRS and LIB3DS_LIBRARIES)
-    set(LIB3DS_FOUND true)
-else(LIB3DS_INCLUDE_DIRS and LIB3DS_LIBRARIES)
-    set(LIB3DS_FOUND FALSE)
-    if(Lib3ds_FIND_REQUIRED)
-        message(FATAL_ERROR "Could not find lib3ds. Please install lib3ds (http://lib3ds.sourceforge.net)")
-    endif(Lib3ds_FIND_REQUIRED)
-endif(LIB3DS_INCLUDE_DIRS and LIB3DS_LIBRARIES)
