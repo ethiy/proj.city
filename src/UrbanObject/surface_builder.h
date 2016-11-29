a3f4da06785996c00171915e25f71c73cd5e1ea0
Ethiy <oussama.ennafii@ign.fr>
2016-11-29 14:18:28 +0100

    bug solved: was adding vertex more than 1 time

--- src/UrbanObject/surface_builder.h
+++ src/UrbanObject/surface_builder.h
@@ -18,15 +18,16 @@ public:
     {
         CGAL::Polyhedron_incremental_builder_3<HDS> incremental_builder( target, true);
         incremental_builder.begin_surface(points.size(), faces.size());
+        std::for_each( std::begin(points), std::end(points), [&](std::pair<size_t, Point> p)
+                                                                {
+                                                                    incremental_builder.add_vertex( p.second);
+                                                                }
+                    );
         std::for_each( std::begin(faces), std::end(faces), [&](Lib3dsFace face)
                                                             {
                                                                 Point point_0 = points[face.points[0]];
                                                                 Point point_1 = points[face.points[1]];
                                                                 Point point_2 = points[face.points[2]];
-
-                                                                incremental_builder.add_vertex( point_0);
-                                                                incremental_builder.add_vertex( point_1);
-                                                                incremental_builder.add_vertex( point_2);
 
                                                                 Vector v1 = Vector(point_0, point_1);
                                                                 Vector v2 = Vector(point_1, point_2);
@@ -37,15 +38,15 @@ public:
                                                                 incremental_builder.begin_facet();
                                                                 if(CGAL::determinant(v1, v2, n)>0)
                                                                 {
-                                                                    incremental_builder.add_vertex_to_facet( 0);
-                                                                    incremental_builder.add_vertex_to_facet( 1);
-                                                                    incremental_builder.add_vertex_to_facet( 2);
+                                                                    incremental_builder.add_vertex_to_facet( face.points[0]);
+                                                                    incremental_builder.add_vertex_to_facet( face.points[1]);
+                                                                    incremental_builder.add_vertex_to_facet( face.points[2]);
                                                                 }
                                                                 else
                                                                 {
-                                                                    incremental_builder.add_vertex_to_facet( 0);
-                                                                    incremental_builder.add_vertex_to_facet( 2);
-                                                                    incremental_builder.add_vertex_to_facet( 1);
+                                                                    incremental_builder.add_vertex_to_facet( face.points[0]);
+                                                                    incremental_builder.add_vertex_to_facet( face.points[2]);
+                                                                    incremental_builder.add_vertex_to_facet( face.points[1]);
                                                                 }
                                                                 incremental_builder.end_facet();
                                                             }
