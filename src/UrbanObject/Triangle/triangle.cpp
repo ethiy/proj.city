0739cdb589202b9c502126b8d9fa5fc1cf620ef1
Ethiy <oussama.ennafii@ign.fr>
2016-12-01 16:34:06 +0100

    implementation triangle

---
+++ src/UrbanObject/Triangle/triangle.cpp
@@ -0,0 +1,14 @@
+#include "triangle.h"
+
+namespace UrbanObject
+{
+    Triangle::Triangle(void){}
+    Triangle::Triangle(const Triangle & other):points(other.points){}
+    Triangle::Triangle(size_t first, size_t second, size_t third):points(std::make_tuple(first, second, third)){}
+    Triangle::~Triangle(void){}
+
+    void Triangle::invert_orientation(void)
+    {
+        points = std::make_tuple(points[0], points[2], points[1]);
+    }
+}
