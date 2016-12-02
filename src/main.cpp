23fd7c0ea1990733dc9d7cc7200abe8645eed140
Ethiy <oussama.ennafii@ign.fr>
2016-12-02 14:21:29 +0100

    minor fix main

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,23 @@
 
 int main(int, char**)
 {
-    boost::filesystem::path filepath("../3DSceneModel/ressources/3dModels/3DS/Toy/Toy Santa Claus N180816.3DS");
+    boost::filesystem::path filepath("/home/ethiy/Workspace/GitHub/3DSceneModel/ressources/3dModels/3DS/Toy/Toy Santa Claus N180816.3DS");
     std::vector<std::string> flags;
-    Reader<Lib3dsFile> reader(filepath, flags);
     std::vector<urban::_Mesh> meshes;
+
+    Reader<Lib3dsFile> reader(filepath, flags);
+    int exit_code = reader.get_exit_code();
     reader.get_facets(meshes);
+
+    std::copy(std::begin(meshes), std::end(meshes), std::ostream_iterator<urban::_Mesh>(std::cout, "\n"));
+
     std::vector<urban::UrbanObject> urban_objects;
-
-    std::transform(std::begin(meshes), std::end(meshes), std::begin(urban_objects), [&](urban::_Mesh mesh)
-                                                                                    {
-                                                                                        return urban::UrbanObject(mesh);
-                                                                                    }
+    std::for_each(std::begin(meshes), std::end(meshes), [&](urban::_Mesh mesh)
+                                                            {
+                                                                urban_objects.push_back(urban::UrbanObject(mesh));
+                                                            }
                 );
-
     std::copy(std::begin(urban_objects), std::end(urban_objects), std::ostream_iterator<urban::UrbanObject>(std::cout, "\n"));
-    return EXIT_SUCCESS;
+
+    return exit_code;
 }
