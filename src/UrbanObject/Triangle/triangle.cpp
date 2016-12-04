212c4aa8cf9e9a099efd1cb330411d53fa057f63
ETHIY <oussama.ennafii@polytechnique.org>
2016-12-04 16:16:30 +0100

    resolving clang warnings 2

--- src/UrbanObject/Triangle/triangle.cpp
+++ src/UrbanObject/Triangle/triangle.cpp
@@ -7,7 +7,7 @@ namespace urban
 {
     Triangle::Triangle(void){}
     Triangle::Triangle(const Triangle & other):points(other.points){}
-    Triangle::Triangle(size_t first, size_t second, size_t third):points{first, second, third}{}
+    Triangle::Triangle(size_t first, size_t second, size_t third):points{{first, second, third}}{}
     Triangle::~Triangle(void){}
 
     size_t Triangle::operator[](size_t index)
@@ -17,7 +17,7 @@ namespace urban
 
     void Triangle::invert_orientation(void)
     {
-        points = {points[0], points[2], points[1]};
+        points = {{points[0], points[2], points[1]}};
     }
 
     std::ostream& operator<<(std::ostream & os, const Triangle & triangle)
