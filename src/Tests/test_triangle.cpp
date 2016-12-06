bd3bc54dd6ffd7b821ccb3644804dc622f993445
Ethiy <oussama.ennafii@ign.fr>
2016-12-06 11:59:36 +0100

    junit & xml reporters work with Ctest

--- src/Tests/test_triangle.cpp
+++ src/Tests/test_triangle.cpp
@@ -19,5 +19,31 @@ SCENARIO("Triangle manipulation:")
                 REQUIRE( auxilary.str() == "145 45 97 " );
             }
         }
+
+        WHEN( "the triangle is copied")
+        {
+            urban::Triangle facet(indexes[0], indexes[1], indexes[2]);
+            urban::Triangle facet2 = facet;
+
+            THEN("the output checks")
+            {
+                std::ostringstream auxilary;
+                auxilary << facet;
+                REQUIRE( auxilary.str() == "145 45 97 " );
+            }
+        }
+
+        WHEN( "the triangle is inverted")
+        {
+            urban::Triangle facet(indexes[0], indexes[1], indexes[2]);
+            facet.invert_orientation();
+
+            THEN("the output checks")
+            {
+                std::ostringstream auxilary;
+                auxilary << facet;
+                REQUIRE( auxilary.str() == "145 97 45 " );
+            }
+        }
     }
 }
