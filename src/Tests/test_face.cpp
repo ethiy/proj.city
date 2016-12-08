85e8ead8b089b934d2c25f4a8ab2fe2b1ad1dad8
Ethiy <oussama.ennafii@ign.fr>
2016-12-08 18:38:22 +0100

    new test : case four points face

--- src/Tests/test_face.cpp
+++ src/Tests/test_face.cpp
@@ -54,5 +54,56 @@ SCENARIO("Face manipulation:")
             auxilary << face_3ds->points[0] << " " << face_3ds->points[1] << " " << face_3ds->points[2];
             REQUIRE( auxilary.str() == "145 45 97" );
         }
+
+    GIVEN( "Four indices:" )
+    {
+        size_t indexes[4] = {145,45,97,85};
+
+        WHEN( "the triangle is created")
+        {
+            urban::Face facet(indexes);
+
+            THEN("the output checks")
+            {
+                std::ostringstream auxilary;
+                auxilary << facet;
+                REQUIRE( auxilary.str() == "4 145 45 97 85 " );
+            }
+        }
+
+        WHEN( "the triangle is copied")
+        {
+            urban::Face facet(indexes);
+            urban::Face facet2 = facet;
+
+            THEN("the output checks")
+            {
+                std::ostringstream auxilary;
+                auxilary << facet;
+                REQUIRE( auxilary.str() == "4 145 45 97 85 " );
+            }
+        }
+
+        WHEN( "the triangle is inverted")
+        {
+            urban::Face facet(indexes);
+            facet.invert_orientation();
+
+            THEN("the output checks")
+            {
+                std::ostringstream auxilary;
+                auxilary << facet;
+                REQUIRE( auxilary.str() == "4 145 85 97 45 " );
+            }
+        }
+
+        WHEN( "the triangle is transformed to Lib3dsFace")
+        {
+            urban::Face facet(indexes);
+            Lib3dsFace* face_3ds = facet.to_3ds();
+            std::ostringstream auxilary;
+            auxilary << face_3ds->points[0] << " " << face_3ds->points[1] << " " << face_3ds->points[2] << " and " << (face_3ds + 1)->points[0] << " " << (face_3ds + 1)->points[1] << " " << (face_3ds + 1)->points[2];
+            REQUIRE( auxilary.str() == "145 45 97 and 145 97 85" );
+        }
     }
 }
