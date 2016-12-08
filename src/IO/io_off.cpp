18485c49178b9c8a5c5f90c0d4ba072e1ea95760
Ethiy <oussama.ennafii@ign.fr>
2016-12-08 14:57:09 +0100

    io off implem skecth

--- src/IO/io_off.cpp
+++ src/IO/io_off.cpp
@@ -1,21 +1,37 @@
 #include "io_off.h"
 
-FileHandler<std::ifstream>::FileHandler(void){}
+#include <cassert>
 
-FileHandler<std::ifstream>::FileHandler(boost::filesystem::path _filepath, std::map<std::string, bool> _modes)
+FileHandler<std::fstream>::FileHandler(void){}
+
+FileHandler<std::fstream>::FileHandler(boost::filesystem::path _filepath, std::map<std::string, bool> _modes)
 :filepath(_filepath), modes(_modes){}
 
-FileHandler<std::ifstream>::~FileHandler(void){}
+FileHandler<std::fstream>::~FileHandler(void){}
 
-int FileHandler<std::ifstream>::read(std::vector<urban::Mesh> & meshes)
+int FileHandler<std::fstream>::read(std::vector<urban::Mesh> & meshes)
 {
     try
     {
-        if(modes["read"]==true)
+        if(modes["read"])
         {
             if(boost::filesystem::is_regular_file(filepath))
             {
-                
+                if(modes["binary"])
+                {
+                    file.open(filepath.string().c_str(), std::ios::in | std::ios::binary);
+                    // ERROR if file is not binary
+                    std::cerr << "Binary reader not yet implemented!" << std::endl;
+                    assert(0);
+                    file.close();
+                }
+                else
+                {
+                    file.open(filepath.string().c_str(), std::ios::in);
+                    // ERROR not handled if file is binary
+                    
+                    file.close();
+                }
             }
             else
             {
