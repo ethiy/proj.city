9055e83aa435a9049ed0ba51732c4274b10b93b5
Ethiy <oussama.ennafii@ign.fr>
2016-12-08 16:55:29 +0100

    needs faces and points parser

--- src/IO/io_off.cpp
+++ src/IO/io_off.cpp
@@ -4,6 +4,15 @@
 
 #include <cassert>
 
+#include <string>
+#include <sstream>
+
+#include <map>
+#include <vector>
+
+#include <algorithm>
+#include <iterator>
+
 namespace urban
 {
     namespace io
@@ -35,7 +44,43 @@ namespace urban
                         {
                             file.open(filepath.string().c_str(), std::ios::in);
                             // ERROR not handled if file is binary
-                            
+                            std::vector<std::string> lines;
+                            read_lines(file, std::back_inserter(lines));
+                            assert(!lines.empty());
+                            std::copy_if(std::begin(lines), std::end(lines), std::back_inserter(lines),
+                                            [](const std::string line)
+                                            {
+                                                return line.at(0) != '#' || !line.empty();
+                                            } 
+                                        );
+                            assert(!lines.empty());
+                            if(lines[0] == "OFF")
+                            {
+                                assert(!lines.empty());
+                                std::vector<size_t> sizes;
+                                {
+                                    std::istringstream _sizes(lines[1]);
+                                    std::copy(std::istream_iterator<size_t>(_sizes), std::istream_iterator<size_t>(), std::begin(sizes));
+                                    assert(sizes.size()==3);
+                                    assert(sizes[2] == 0);
+                                    assert(lines.size() == (2+sizes[0]+sizes[1])); // assuming the last empty line is not counted!!!
+                                }
+                                std::map<size_t, urban::Point> points;
+                                std::for_each(std::begin(lines) + 2, std::begin(lines) + sizes[0],
+                                                [&](std::string line)
+                                                {
+                                                    return;
+                                                } 
+                                            );
+                                
+                                // Faces to parse
+                            }
+                            else
+                            {
+                                boost::system::error_code ec(boost::system::errc::no_such_file_or_directory, boost::system::system_category());
+                                exit_code = boost::system::errc::no_such_file_or_directory;
+                                throw boost::filesystem::filesystem_error(ec.message(), ec);
+                            }
                             file.close();
                         }
                     }
