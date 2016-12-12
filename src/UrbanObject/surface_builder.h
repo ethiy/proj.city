f2793ef5e579f25a2b1989f76b56876061065c41
Ethiy <oussama.ennafii@ign.fr>
2016-12-12 14:32:20 +0100

    using Face:: iterators  in surface builder

--- src/UrbanObject/surface_builder.h
+++ src/UrbanObject/surface_builder.h
@@ -14,36 +14,43 @@ namespace urban
     class SurfaceBuilder: public CGAL::Modifier_base<HDS>
     {
     public:
-        SurfaceBuilder(ShadowMesh _mesh): mesh(_mesh){}
+        SurfaceBuilder(ShadowMesh _shadow_mesh): shadow_mesh(_shadow_mesh){}
         void operator()(HDS & target)
         {
-            std::map<size_t, Point> points = mesh.get_points();
-            std::map<size_t, Face> faces = mesh.get_faces();
+            std::map<size_t, Point> points = shadow_mesh.get_points();
+            std::map<size_t, Face> faces = shadow_mesh.get_faces();
 
             CGAL::Polyhedron_incremental_builder_3<HDS> incremental_builder( target, true);
             incremental_builder.begin_surface(points.size(), faces.size());
 
-            std::for_each( std::begin(points), std::end(points), [&](std::pair<size_t, Point> p)
-                                                                    {
-                                                                        incremental_builder.add_vertex( p.second);
-                                                                    }
-                        );
-            std::for_each( std::begin(faces), std::end(faces), [&](std::pair<size_t, Face> face)
-                                                                {
-                                                                    incremental_builder.begin_facet();
-                                                                    std::vector<size_t> indexes = face.second.get_indexes();
-                                                                    std::for_each(std::begin(indexes), std::end(indexes), 
-                                                                                [&](size_t index)
-                                                                                {
-                                                                                    incremental_builder.add_vertex_to_facet(face.second[index]); // Need a nested iterator!!
-                                                                                } 
-                                                                                );
-                                                                    incremental_builder.end_facet();
-                                                                }
-                        );
+            std::for_each(
+                std::begin(points),
+                std::end(points),
+                [&](std::pair<size_t, Point> p)
+                {
+                    incremental_builder.add_vertex( p.second);
+                }
+            );
+            std::for_each(
+                std::begin(faces),
+                std::end(faces),
+                [&](std::pair<size_t, Face> face)
+                {
+                    incremental_builder.begin_facet();
+                    std::for_each(
+                        std::begin(face.second),
+                        std::end(face.second),
+                        [&](size_t index)
+                        {
+                            incremental_builder.add_vertex_to_facet(face.second[index]);
+                        } 
+                    );
+                    incremental_builder.end_facet();
+                }
+            );
             incremental_builder.end_surface();
         }
     private:
-        ShadowMesh mesh;
+        ShadowMesh shadow_mesh;
     };
 }
