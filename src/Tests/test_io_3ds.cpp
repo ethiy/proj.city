7c73a299a3e0dc7201e77bab4f4484f9d743d802
Ethiy <oussama.ennafii@ign.fr>
2016-12-12 15:11:00 +0100

    init 3ds io test

---
+++ src/Tests/test_io_3ds.cpp
@@ -0,0 +1,11 @@
+#include "../IO/io_3ds.h"
+
+#include "catch.hpp"
+
+SCENARIO("Input/Output from 3dsMAX file:")
+{
+    GIVEN("A urban::ShadowMesh object")
+    {
+        urban::ShadowMesh shadow_mesh;
+    }
+}
