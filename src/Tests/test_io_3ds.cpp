22fdc2d2ec116f2c87567e8f8233f17b3f328306
Ethiy <oussama.ennafii@ign.fr>
2016-12-12 15:38:17 +0100

    test read 3ds

--- src/Tests/test_io_3ds.cpp
+++ src/Tests/test_io_3ds.cpp
@@ -1,11 +1,35 @@
 #include "../IO/io_3ds.h"
 
+#include <boost/filesystem.hpp>
+
+#include <string>
+#include <fstream>
+#include <streambuf>
+
 #include "catch.hpp"
 
 SCENARIO("Input/Output from 3dsMAX file:")
 {
-    GIVEN("A urban::ShadowMesh object")
+    GIVEN("An existing 3dsMAX file")
     {
-        urban::ShadowMesh shadow_mesh;
+        boost::filesystem::path filepath("../../ressources/3dModels/3DS/Toy/Toy Santa Claus N180816.3DS");
+        
+        WHEN("the reading mode is chosen")
+        {
+            std::map<std::string,bool> modes{{"read", true}};
+            urban::io::FileHandler<Lib3dsFile> handler(filepath, modes);
+            std::vector<urban::ShadowMesh> meshes;
+            int exit_code = handler.read(meshes);
+
+            THEN("the output checks")
+            {
+                std::ostringstream auxilary;
+                std::copy(std::begin(meshes), std::end(meshes), std::ostream_iterator<urban::ShadowMesh>(auxilary, "\n"));
+
+                std::ifstream tmp("../../ressources/tests/santa_shadow_mesh.txt");
+                std::string tmp_str((std::istreambuf_iterator<char>(tmp)), std::istreambuf_iterator<char>());
+                REQUIRE( auxilary.str() == tmp_str );
+            }
+        }
     }
 }
