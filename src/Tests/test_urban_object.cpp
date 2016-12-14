a2f80b2dd68fc71b9f9b51d435eec50c4c3cac33
Ethiy <oussama.ennafii@ign.fr>
2016-12-14 15:15:53 +0100

    first test for urban object

--- src/Tests/test_urban_object.cpp
+++ src/Tests/test_urban_object.cpp
@@ -1,4 +1,11 @@
 #include "../UrbanObject/urban_object.h"
+#include "../IO/io_off.h"
+
+#include <boost/filesystem.hpp>
+
+#include <string>
+#include <fstream>
+#include <streambuf>
 
 #include "catch.hpp"
 
@@ -6,6 +13,24 @@ SCENARIO("UrbanObject manipulation:")
 {
     GIVEN("A urban::ShadowMesh object")
     {
-        urban::ShadowMesh shadow_mesh;
+        std::map<std::string,bool> modes{{"read", true}};
+        boost::filesystem::path filepath("../../ressources/3dModels/OFF/hammerhead.off");
+        urban::io::FileHandler<std::fstream> handler(filepath, modes);
+        urban::ShadowMesh mesh = handler.read();
+
+        WHEN("the UrbanObject is constructed")
+        {
+            urban::UrbanObject hammerhead(mesh);
+
+            THEN("the output checks")
+            {
+                std::ostringstream auxilary;
+                auxilary << hammerhead;
+
+                std::ifstream tmp("../../ressources/tests/hammerhead.off");
+                std::string tmp_str((std::istreambuf_iterator<char>(tmp)), std::istreambuf_iterator<char>());
+                REQUIRE( auxilary.str() == tmp_str );
+            }
+        }
     }
 }
