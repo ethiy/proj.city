b5369c90f9f0630f3dde54ee260493091fbe19d2
Ethiy <oussama.ennafii@ign.fr>
2016-12-14 13:17:13 +0100

    catch in main

--- src/main.cpp
+++ src/main.cpp
@@ -14,45 +14,44 @@
 #include <iterator>
 #include <iostream>
 
-int main(int, char**)
+int main(int, char **)
 {
-    boost::filesystem::path filepath("../../ressources/3dModels/3DS/Toy/Toy Santa Claus N180816.3DS");
-    std::map<std::string,bool> modes{{"read", true}};
-    urban::io::FileHandler<Lib3dsFile> handler(filepath, modes);
-    std::vector<urban::ShadowMesh> meshes;
-    int exit_code = handler.read(meshes);
-
-    std::copy(std::begin(meshes), std::end(meshes), std::ostream_iterator<urban::ShadowMesh>(std::cout, "\n"));
-
-    std::vector<urban::UrbanObject> urban_objects;
-    std::transform(
-        std::begin(meshes),
-        std::end(meshes),
-        std::back_inserter(urban_objects),
-        [](urban::ShadowMesh mesh)
-        {
-            return urban::UrbanObject(mesh);
-        }
-    );
-
-    std::copy(std::begin(urban_objects), std::end(urban_objects), std::ostream_iterator<urban::UrbanObject>(std::cout, "\n"));
-    
-    if(!exit_code)
+    try
     {
+        boost::filesystem::path filepath("../../ressources/3dModels/3DS/Toy/Toy Santa Claus N180816.3DS");
+        std::map<std::string, bool> modes{{"read", true}};
+        urban::io::FileHandler<Lib3dsFile> handler(filepath, modes);
+        std::vector<urban::ShadowMesh> meshes = handler.read();
+
+        std::copy(std::begin(meshes), std::end(meshes), std::ostream_iterator<urban::ShadowMesh>(std::cout, "\n"));
+
+        std::vector<urban::UrbanObject> urban_objects;
+        std::transform(
+            std::begin(meshes),
+            std::end(meshes),
+            std::back_inserter(urban_objects),
+            [](urban::ShadowMesh mesh)
+            {
+                return urban::UrbanObject(mesh);
+            }
+        );
+
+        std::copy(std::begin(urban_objects), std::end(urban_objects), std::ostream_iterator<urban::UrbanObject>(std::cout, "\n"));
         CGAL::Geomview_stream geomview_stream;
         std::for_each(
             std::begin(urban_objects),
             std::end(urban_objects),
-            [&](urban::UrbanObject obj)
-            {
+            [&](urban::UrbanObject obj) {
                 geomview_stream << obj;
-            }
-        );
-        
+            });
+
         geomview_stream.look_recenter();
         std::cout << "Enter any character to stop the program:" << std::endl;
         getchar();
     }
-
-    return exit_code;
+    catch (const std::exception& except)
+    {
+        std::cerr << except.what() << std::endl;
+    }
+    return EXIT_SUCCESS;
 }
