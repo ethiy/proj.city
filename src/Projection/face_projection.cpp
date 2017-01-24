5bc29d56a5f83b58e51e6a4d9de1839572ff8fa2
Ethiy <oussama.ennafii@ign.fr>
2017-01-24 18:48:58 +0100

    intersections not yet finished

--- src/Projection/face_projection.cpp
+++ src/Projection/face_projection.cpp
@@ -1,6 +1,9 @@
 #include "face_projection.h"
 
 #include <algorithm>
+#include <iterator>
+
+#include <stdexcept>
 
 namespace urban
 {
@@ -31,7 +34,7 @@ namespace urban
 
     bool FaceProjection::contains(const FaceProjection & facet)
     {
-        return std::all_of(
+        bool inside = std::all_of(
             facet.vertices_begin(),
             facet.vertices_end(),
             [this](const Point_2 & point)
@@ -39,6 +42,53 @@ namespace urban
                 return contains(point);
             }
         );
+
+        bool non_inside = std::none_of(
+            facet.vertices_begin(),
+            facet.vertices_end(),
+            [this](const Point_2 & point)
+            {
+                return contains(point);
+            }
+        );
+        return inside || (non_inside && contains(CGAL::centroid(facet(0), facet(1), facet(2)))); // prone to problems if centroid is on boundary!!
+    }
+
+    std::pair<std::vector<size_t>, std::vector<size_t> > FaceProjection::overlap(const FaceProjection & facet)
+    {
+        std::vector<size_t> starts(facet.size()), lasts(facet.size());
+
+        size_t circulator(0);
+        while(circulator < facet.size())
+        {
+            if(( circulator > 0 && !contains(facet[circulator - 1]) && contains(facet[circulator]) ) || ( circulator == 0 && !contains(facet[facet.size() - 1]) && contains(facet[0]) ))
+                starts.push_back(circulator);
+            if(( circulator < facet.size()-2 && contains(facet[circulator]) && !contains(facet[circulator + 1]) ) || ( circulator == facet.size()-1 && contains(facet[facet.size() - 1]) && !contains(facet[0]) ))
+                lasts.push_back(circulator);
+        }
+
+        if(starts.size()!=lasts.size())
+            throw new std::out_of_range("There should be the same number of starting and lasting vertices!");
+
+        if( lasts[0] < starts[0] )
+            std::rotate(std::begin(lasts), std::next(std::begin(lasts), 1) , std::end(lasts));
+
+        return std::pair<std::vector<size_t>, std::vector<size_t> >(starts, lasts);
+    }
+
+    std::vector<Point_2> intersect(const Segment_2 & segment)
+    {
+        std::vector<Point_2> intersections;
+        std::transform(
+            edges_begin(),
+            edges_end(),
+            std::back_inserter(intersections),
+            [& segment](const Segment_2 & edge)
+            {
+                CGAL::cpp11::result_of<Segment_2, Segment_2>::type result = intersection(segment, edge);
+            }
+        );
+        return intersections;
     }
 
     double FaceProjection::get_height(const Point_2 & point)
--- src/Projection/face_projection.h
+++ src/Projection/face_projection.h
@@ -2,6 +2,9 @@
 
 #include "../geometry_definitions.h"
 
+#include <vector>
+#include <utility>
+
 namespace urban
 {
     class FaceProjection : Polygon
@@ -9,10 +12,12 @@ namespace urban
     public:
         Plane get_plane(void) const noexcept;
         Vector get_normal(void) const noexcept;
-        double get_height(const Point_2 &); // problem: degenerate faces and intersections
+        double get_height(const Point_2 &); // problem: degenerate faces
 
         bool contains(const Point_2 &);
         bool contains(const FaceProjection &);
+        std::pair<std::vector<size_t>, std::vector<size_t> > overlap(const FaceProjection &);
+        Point_2 intersect(const Segment_2 &);
 
         void set_plane(const Plane & _plane) noexcept;
     private:
