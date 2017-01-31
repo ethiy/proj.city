e62d3287c2a0c7c8647b3fa1627199e435905ee5
Ethiy <oussama.ennafii@ign.fr>
2017-01-31 12:17:52 +0100

    lib3ds -> libfree3ds

--- cmake/modules/free3ds.cmake
+++ cmake/modules/free3ds.cmake
@@ -1,25 +1,17 @@
 if(NOT TARGET free3ds)
     include(ExternalProject)
-    ExternalProject_Add(free3ds
+    ExternalProject_Add(libfree3ds
         PREFIX ${CMAKE_BINARY_DIR}/external/free3ds
         GIT_REPOSITORY "https://github.com/Ethiy/free3ds.git"
-        GIT_TAG 834c6579c6768a73dca41b7c14e9daba4491ebce
-        UPDATE_COMMAND ""
-        INSTALL_DIR ${CMAKE_BINARY_DIR}/external/free3ds
-        CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        GIT_TAG 24cc51d8677b779d84600d6ffa1b7afc1058a4cf
+        CMAKE_ARGS  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                     -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/external/free3ds
-                    -DBUILD_STATIC_LIBS=ON
-                    -DBUILD_PACKAGING=OFF
-                    -DBUILD_TESTING=OFF
-                    -DBUILD_NC_TESTS=OFF
-                    -BUILD_CONFIG_TESTS=OFF
-                    -DINSTALL_HEADERS=ON
                     -DCMAKE_C_FLAGS=${CMAKE_CXX_FLAGS}
                     -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-        LOG_DOWNLOAD 1
-        LOG_INSTALL 1
+        BUILD_COMMAND make
+        INSTALL_COMMAND make install
     )
 
     set(LIB3DS_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/external/free3ds/include)
-    set(LIB3DS_LIBRARIES ${CMAKE_BINARY_DIR}/external/free3ds/lib/libfree3ds.a )
+    set(LIB3DS_LIBRARIES ${CMAKE_BINARY_DIR}/external/free3ds/lib/libfree3ds.so )
 endif()
