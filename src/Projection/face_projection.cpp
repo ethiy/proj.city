c503f05f9ed5b96bf781f201e54b1fa58089d12b
Ethiy <oussama.ennafii@ign.fr>
2017-01-31 14:30:01 +0100

    face proj. :inheritance -> encapsulation

--- src/Projection/face_projection.cpp
+++ src/Projection/face_projection.cpp
@@ -7,33 +7,36 @@
 
 namespace urban
 {
-    FaceProjection::FaceProjection(const FaceProjection & other):Polygon(other), original_plane(other.original_plane){}
+    FaceProjection::FaceProjection(void){}
+    FaceProjection::FaceProjection(const Polygon & _projected_polygon, const Plane & _supporting_plane):projected_polygon(_projected_polygon), supporting_plane(_supporting_plane){}
+    FaceProjection::FaceProjection(const FaceProjection & other):projected_polygon(other.projected_polygon), supporting_plane(other.supporting_plane){}
+    FaceProjection::~FaceProjection(void){}
 
     Plane FaceProjection::get_plane(void) const noexcept
     {
-        return original_plane;
+        return supporting_plane;
     }
 
     Vector FaceProjection::get_normal(void) const noexcept
     {
-        return Vector(original_plane.a(), original_plane.b(), original_plane.c());
+        return Vector(supporting_plane.a(), supporting_plane.b(), supporting_plane.c());
     }
 
     double FaceProjection::get_height(const Point_2 & point)
     {
-        return !is_degenerate() * contains(point) * to_double(( -1 * original_plane.d() - original_plane.a() * point.x() - original_plane.b() * point.y()) / original_plane.c()) ;
+        return !is_degenerate() * contains(point) * to_double(( -1 * supporting_plane.d() - supporting_plane.a() * point.x() - supporting_plane.b() * point.y()) / supporting_plane.c()) ;
     }
 
 
     bool FaceProjection::is_degenerate(void) const
     {
-        return area() == 0; // In our case, two edges are coinciding means that all edges should be!
+        return projected_polygon.area() == 0;
     }
 
     bool FaceProjection::contains(const Point_2 & point)
     {
         bool inside(false);
-        switch (bounded_side(point))
+        switch (projected_polygon.bounded_side(point))
         {
             case CGAL::ON_BOUNDED_SIDE:
                 inside = true; break;
@@ -43,10 +46,5 @@ namespace urban
                 break;
         }
         return inside;
-    }
-
-    void FaceProjection::set_plane(const Plane & plane) noexcept
-    {
-        original_plane = plane;
     }
 }
--- src/Projection/face_projection.h
+++ src/Projection/face_projection.h
@@ -7,23 +7,28 @@
 
 namespace urban
 {
-    class FaceProjection : Polygon
+    class FaceProjection
     {
     public:
-        using Polygon::Polygon;
-
+        FaceProjection(void);
+        FaceProjection(const Polygon &, const Plane &);
         FaceProjection(const FaceProjection &);
+        ~FaceProjection(void);
 
         Plane get_plane(void) const noexcept;
         Vector get_normal(void) const noexcept;
         double get_height(const Point_2 &);
-
+        
+        /*! In our case, two edges are coinciding means that all edges are so:
+         *  - Due to the fact that all edges are supported by the same plane;
+         *  - This means that we should just check if the area is null to assert
+         *      that the projection is degenarate.
+         */
         bool is_degenerate(void) const;
 
         bool contains(const Point_2 &);
-
-        void set_plane(const Plane & _plane) noexcept;
     private:
-        Plane original_plane;
+        Polygon projected_polygon;
+        Plane supporting_plane;
     };
 }
