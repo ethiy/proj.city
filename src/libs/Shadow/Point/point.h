7f7d239ebce1db573af033aebe428f7bfd90eb80
Ethiy <oussama.ennafii@ign.fr>
2017-02-20 14:39:00 +0100

    bug catch shadow point

--- src/libs/Shadow/Point/point.h
+++ src/libs/Shadow/Point/point.h
@@ -18,7 +18,7 @@ namespace urban
             Point(void);
             Point(double x, double y, double z);
             Point(double _coordinates[3]);
-            template<class Kernel> Point(const CGAL::Point_3<Kernel &> point);
+            template<class Kernel> Point(const CGAL::Point_3<Kernel> & point);
             Point(const Point & other);
             Point(Point && other);
             ~Point(void);
