d52721204439ad92bed70b3861fc5979a022eb6c
ETHIY <oussama.ennafii@polytechnique.org>
2017-02-22 20:51:12 +0100

    cmake free3ds module bug catch

--- cmake/modules/free3ds.cmake
+++ cmake/modules/free3ds.cmake
@@ -6,12 +6,12 @@ if(NOT TARGET free3ds)
         GIT_TAG 24cc51d8677b779d84600d6ffa1b7afc1058a4cf
         CMAKE_ARGS  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                     -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/external/free3ds
-                    -DCMAKE_C_FLAGS=${CMAKE_CXX_FLAGS}
-                    -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+                    -DCMAKE_C_FLAGS= "-O3"
+                    -DCMAKE_CXX_FLAGS="-O3"
         BUILD_COMMAND make
         INSTALL_COMMAND make install
     )
 
     set(LIB3DS_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/external/free3ds/include)
-    set(LIB3DS_LIBRARIES ${CMAKE_BINARY_DIR}/external/free3ds/lib/libfree3ds.so )
+    file(GLOB_RECURSE LIB3DS_LIBRARIES "${CMAKE_BINARY_DIR}/external/free3ds/lib/libfree3ds.*")
 endif()
