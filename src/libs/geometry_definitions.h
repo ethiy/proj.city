63c986e9abaf70c31918f44330f88218dc79b874
Ethiy <oussama.ennafii@ign.fr>
2017-02-22 17:38:47 +0100

    add Polygon set to def

--- src/libs/geometry_definitions.h
+++ src/libs/geometry_definitions.h
@@ -10,6 +10,7 @@
 
 #include <CGAL/Polygon_2.h>
 #include <CGAL/Polygon_with_holes_2.h>
+#include <CGAL/Polygon_set_2.h>
 
 #include <CGAL/intersections.h>
 
@@ -49,6 +50,7 @@ namespace urban
 
     typedef CGAL::Polygon_2<Kernel> Polygon;
     typedef CGAL::Polygon_with_holes_2<Kernel> Polygon_with_holes;
+    typedef CGAL::Polygon_set_2<Kernel> Polygon_set;
     typedef Kernel::Point_2 Point_2;
     typedef Kernel::Vector_2 Vector_2;
     typedef Kernel::Intersect_2 Intersect_2;
