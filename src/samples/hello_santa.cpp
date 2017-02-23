b931100989da87b74779fce3bf11f016504726f6
Ethiy <oussama.ennafii@ign.fr>
2017-02-23 15:10:43 +0100

    try forking the visualizer

--- src/samples/hello_santa.cpp
+++ src/samples/hello_santa.cpp
@@ -1,16 +1,63 @@
 #include "../libs/urban.h"
 
-#ifdef CGAL_USE_GEOMVIEW
-#include <CGAL/IO/Geomview_stream.h>
-#endif // CGAL_USE_GEOMVIEW
-
 #include <boost/filesystem.hpp>
+#include <thread>
 
 #include <vector>
 #include <map>
 #include <algorithm>
 #include <iterator>
 #include <iostream>
+
+#ifdef CGAL_USE_GEOMVIEW
+#include <CGAL/IO/Geomview_stream.h>
+
+class GeomviewVisualizer
+{
+public:
+    inline GeomviewVisualizer(void){}
+    inline GeomviewVisualizer(const std::vector<urban::Brick> & _urban_objects): urban_objects(_urban_objects) {}
+    inline GeomviewVisualizer(const GeomviewVisualizer & other): geomview_stream(other.geomview_stream), urban_objects(other.urban_objects) {}
+    inline GeomviewVisualizer(GeomviewVisualizer && other): geomview_stream(std::move(other.geomview_stream)), urban_objects(std::move(urban_objects)) {}
+    inline ~GeomviewVisualizer(void){}
+
+    inline GeomviewVisualizer & operator=(const GeomviewVisualizer & other) noexcept
+    {
+        geomview_stream = other.geomview_stream;
+        urban_objects = other.urban_objects;
+        return *this;
+    }
+
+    inline GeomviewVisualizer & operator=(GeomviewVisualizer && other) noexcept
+    {
+        geomview_stream = std::move(other.geomview_stream);
+        urban_objects = std::move(other.urban_objects);
+        return *this;
+    }
+
+    inline void operator()(void)
+    {
+        geomview_stream.set_bg_color(CGAL::Color(0, 127, 200));
+        size_t pigment(1);
+        size_t all(urban_objects.size());
+        std::for_each(
+        std::begin(urban_objects),
+        std::end(urban_objects),
+        [&pigment, &all, this](const urban::Brick & obj)
+            {
+                geomview_stream << CGAL::Color(250 * (pigment - 1) / all, 0, 250 * pigment / all) << obj;
+                pigment++;
+            });
+
+        geomview_stream.look_recenter();
+    }
+private:
+    CGAL::Geomview_stream geomview_stream;
+    std::vector<urban::Brick> urban_objects;
+};
+
+#endif // CGAL_USE_GEOMVIEW
+
 
 int main(int, char **)
 {
@@ -35,24 +82,13 @@ int main(int, char **)
         );
 
         std::copy(std::begin(urban_objects), std::end(urban_objects), std::ostream_iterator<urban::Brick>(std::cout, "\n"));
-        #ifdef CGAL_USE_GEOMVIEW
-        CGAL::Geomview_stream geomview_stream;
-        geomview_stream.set_bg_color(CGAL::Color(0, 127, 200));
-        size_t pigment(1);
-        size_t all(meshes.size());
-        std::for_each(
-            std::begin(urban_objects),
-            std::end(urban_objects),
-            [&](urban::Brick & obj)
-            {
-                geomview_stream << CGAL::Color(250 * (pigment - 1) / all, 0, 250 * pigment / all) << obj;
-                pigment++;
-            });
 
-        geomview_stream.look_recenter();
+
+        #ifdef CGAL_USE_GEOMVIEW
+        GeomviewVisualizer santa_visualizer(urban_objects);
+        // std::thread geomview_thread(santa_visualizer);
+        // geomview_thread.detach();
         #endif // CGAL_USE_GEOMVIEW
-        std::cout << "Enter any character to stop the program:" << std::endl;
-        getchar();
     }
     catch (const std::exception& except)
     {
