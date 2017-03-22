deb942c574a2d6b1b40dbc5f2e80f2075e8776b1
Ethiy <oussama.ennafii@ign.fr>
2017-03-22 14:46:15 +0100

    prefinal

--- src/libs/algorithms/raster/raster_algorithms.cpp
+++ src/libs/algorithms/raster/raster_algorithms.cpp
@@ -9,17 +9,14 @@ namespace urban
 {
     projection::RasterPrint rasterize(const projection::BrickPrint & brick_projection, const double & pixel_size, shadow::Point pivot)
     {
-        size_t width = std::ceil((brick_projection.bbox().ymax() - brick_projection.bbox().ymin()) / pixel_size);
-        size_t height = std::ceil((brick_projection.bbox().xmax() - brick_projection.bbox().xmin()) / pixel_size);
-
         return std::accumulate(
             brick_projection.cbegin(),
             brick_projection.cend(),
             projection::RasterPrint(
                 brick_projection.get_name(),
                 shadow::Point(pivot.x() + brick_projection.bbox().xmin(), pivot.y() + brick_projection.bbox().ymax(), pivot.z()),
-                height,
-                width,
+                static_cast<size_t>(std::ceil((brick_projection.bbox().ymax() - brick_projection.bbox().ymin()) / pixel_size)),
+                static_cast<size_t>(std::ceil((brick_projection.bbox().xmax() - brick_projection.bbox().xmin()) / pixel_size)),
                 pixel_size
             ),
             [](projection::RasterPrint result, const projection::FacePrint & face_projection)
