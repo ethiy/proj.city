bcc16fd77be16abf906b952183d03079ebfb2c8d
ETHIY <oussama.ennafii@polytechnique.org>
2017-04-14 14:49:49 +0200

    refactoring tests

--- src/tests/test_shadow_face.cpp
+++ src/tests/test_shadow_face.cpp
@@ -8,12 +8,12 @@ SCENARIO("Face manipulation:")
 {
     GIVEN("Two indexes")
     {
-        std::vector<size_t> indexes{{145,45}};
+        urban::shadow::Face facet{{145,45}};
         WHEN("the triangle is created")
         {
             THEN("The constructor throws")
             {
-                REQUIRE_THROWS_AS( urban::shadow::Face(2, indexes), std::out_of_range );
+                REQUIRE_THROWS(facet);
             }
         }
 
@@ -21,35 +21,33 @@ SCENARIO("Face manipulation:")
 
     GIVEN("Three indexes" )
     {
-        size_t indexes[3] = {145,45,97};
         std::map<size_t, urban::shadow::Point> coord{{145, urban::shadow::Point(15.5343, -13.4504, 60.8789)}, {45, urban::shadow::Point(15.7204, -13.188, 60.8789)}, {97, urban::shadow::Point(15.7204, -13.188, 61.1764)}};
-
         WHEN("the triangle is created")
         {
             THEN("the output checks")
             {
                 std::ostringstream auxilary;
-                auxilary << urban::shadow::Face(indexes[0], indexes[1], indexes[2]);
-                REQUIRE( auxilary.str() == "3 145 45 97 " );
+                auxilary << urban::shadow::Face{{145,45,97}};
+                REQUIRE( auxilary.str() == "3 145 45 97" );
             }
         }
 
         WHEN("the triangle is inverted")
         {
-            urban::shadow::Face facet(indexes[0], indexes[1], indexes[2]);
+            urban::shadow::Face facet{{145,45,97}};
             facet.invert_orientation();
 
             THEN("the output checks")
             {
                 std::ostringstream auxilary;
                 auxilary << facet;
-                REQUIRE( auxilary.str() == "3 145 97 45 " );
+                REQUIRE( auxilary.str() == "3 145 97 45" );
             }
         }
 
         WHEN("Convexity is checked")
         {
-            urban::shadow::Face facet(indexes[0], indexes[1], indexes[2]);
+            urban::shadow::Face facet{{145,45,97}};
             THEN("the output checks")
             {
                 REQUIRE( facet.is_convex(coord) );
@@ -58,8 +56,8 @@ SCENARIO("Face manipulation:")
 
         WHEN("the triangle is transformed to Lib3dsFace")
         {
-            urban::shadow::Face facet(indexes[0], indexes[1], indexes[2]);
-            Lib3dsFace* face_3ds(facet.to_3ds(coord));
+            urban::shadow::Face facet{{145,45,97}};
+            Lib3dsFace* face_3ds = facet.to_3ds(coord);
             THEN("the output checks")
             {
                 std::ostringstream auxilary;
@@ -70,50 +68,32 @@ SCENARIO("Face manipulation:")
         }
     }
 
-    GIVEN("Four indices:" )
+    GIVEN("A four indices face:" )
     {
-        std::vector<size_t> indexes{{145,45,97,85}};
+        urban::shadow::Face facet{{145,45,97,85}};
         WHEN("the facet is created")
         {
-            urban::shadow::Face facet(indexes.size(), indexes);
-
             THEN("the output checks")
             {
                 std::ostringstream auxilary;
                 auxilary << facet;
-                REQUIRE( auxilary.str() == "4 145 45 97 85 " );
-            }
-        }
-
-        WHEN("the facet is copied")
-        {
-            urban::shadow::Face facet(indexes.size(), indexes);
-            urban::shadow::Face facet2 = facet;
-
-            THEN("the output checks")
-            {
-                std::ostringstream auxilary;
-                auxilary << facet;
-                REQUIRE( auxilary.str() == "4 145 45 97 85 " );
+                REQUIRE( auxilary.str() == "4 145 45 97 85" );
             }
         }
 
         WHEN("the facet is inverted")
         {
-            urban::shadow::Face facet(indexes.size(), indexes);
             facet.invert_orientation();
-
             THEN("the output checks")
             {
                 std::ostringstream auxilary;
                 auxilary << facet;
-                REQUIRE( auxilary.str() == "4 145 85 97 45 " );
+                REQUIRE( auxilary.str() == "4 145 85 97 45" );
             }
         }
         
         WHEN("Convexity is checked with the wrong number of points")
         {
-            urban::shadow::Face facet(indexes.size(), indexes);
             std::map<size_t, urban::shadow::Point> coord{{145, urban::shadow::Point(15.5343, -13.4504, 60.8789)}, {45, urban::shadow::Point(15.7204, -13.188, 60.8789)}, {97, urban::shadow::Point(15.7204, -13.188, 61.1764)}};
             THEN("It throws")
             {
@@ -123,7 +103,6 @@ SCENARIO("Face manipulation:")
 
         WHEN("Convexity is checked for a non simple polygon")
         {
-            urban::shadow::Face facet(indexes.size(), indexes);
             std::map<size_t, urban::shadow::Point> coord{{145, urban::shadow::Point(15.5343, -13.4504, 60.8789)}, {85, urban::shadow::Point(15.7204, -13.188, 60.8789)}, {97, urban::shadow::Point(15.65, -12.988, 60.8789)}, {45, urban::shadow::Point(15.65, -14.325, 60.8789)}};
             THEN("It does not checkout ")
             {
@@ -133,7 +112,6 @@ SCENARIO("Face manipulation:")
 
         WHEN("Convexity is checked for a convex rectangle")
         {
-            urban::shadow::Face facet(indexes.size(), indexes);
             std::map<size_t, urban::shadow::Point> coord{{145, urban::shadow::Point(15.5343, -13.4504, 60.8789)}, {97, urban::shadow::Point(15.7204, -13.188, 60.8789)}, {85, urban::shadow::Point(15.65, -12.988, 60.8789)}, {45, urban::shadow::Point(15.65, -14.325, 60.8789)}};
             THEN("It does checkout ")
             {
@@ -143,7 +121,6 @@ SCENARIO("Face manipulation:")
 
         WHEN("Convexity is checked for a non convex (simple) rectangle")
         {
-            urban::shadow::Face facet(indexes.size(), indexes);
             std::map<size_t, urban::shadow::Point> coord{{145, urban::shadow::Point(15.5343, -13.4504, 60.8789)}, {97, urban::shadow::Point(15.7204, -13.188, 60.8789)}, {85, urban::shadow::Point(15.65, -14., 60.8789)}, {45, urban::shadow::Point(15.65, -14.325, 60.8789)}};
             THEN("It does checkout ")
             {
@@ -153,7 +130,6 @@ SCENARIO("Face manipulation:")
 
         WHEN("A convex facet is transformed to Lib3dsFace")
         {
-            urban::shadow::Face facet(indexes.size(), indexes);
             std::map<size_t, urban::shadow::Point> coord{{145, urban::shadow::Point(15.5343, -13.4504, 60.8789)}, {97, urban::shadow::Point(15.7204, -13.188, 60.8789)}, {85, urban::shadow::Point(15.65, -12.988, 60.8789)}, {45, urban::shadow::Point(15.65, -14.325, 60.8789)}};
             Lib3dsFace* face_3ds = facet.to_3ds(coord);
             THEN("the output checks")
--- src/tests/test_shadow_mesh.cpp
+++ src/tests/test_shadow_mesh.cpp
@@ -44,7 +44,7 @@ SCENARIO("shadow::Mesh manipulation:")
             {
                 std::ostringstream auxilary;
                 auxilary << u_mesh;
-                REQUIRE( auxilary.str() == "Name: \nBounding box: 15.5343 15.7204 -13.4504 -13.188 60.8789 61.1764\nPoints: \nPoint 0 : 15.5343 -13.4504 60.8789\nPoint 1 : 15.7204 -13.188 60.8789\nPoint 2 : 15.7204 -13.188 61.1764\nFaces: \nFace 0 : 3 0 2 1 \n" );
+                REQUIRE( auxilary.str() == "Name: \nBounding box: 15.5343 15.7204 -13.4504 -13.188 60.8789 61.1764\nPoints: \nPoint 0 : 15.5343 -13.4504 60.8789\nPoint 1 : 15.7204 -13.188 60.8789\nPoint 2 : 15.7204 -13.188 61.1764\nFaces: \nFace 0 : 3 0 2 1\n" );
             }
         }
         WHEN( "mesh points and faces are accessed:")
@@ -90,7 +90,7 @@ SCENARIO("shadow::Mesh manipulation:")
                 urban::shadow::Mesh _u_mesh(_mesh);
                 std::free(_mesh);
                 auxilary << _u_mesh;
-                REQUIRE( auxilary.str() == "Name: \nBounding box: 15.5343 15.7204 -13.4504 -13.188 60.8789 61.1764\nPoints: \nPoint 0 : 15.5343 -13.4504 60.8789\nPoint 1 : 15.7204 -13.188 60.8789\nPoint 2 : 15.7204 -13.188 61.1764\nFaces: \nFace 0 : 3 0 2 1 \n" );
+                REQUIRE( auxilary.str() == "Name: \nBounding box: 15.5343 15.7204 -13.4504 -13.188 60.8789 61.1764\nPoints: \nPoint 0 : 15.5343 -13.4504 60.8789\nPoint 1 : 15.7204 -13.188 60.8789\nPoint 2 : 15.7204 -13.188 61.1764\nFaces: \nFace 0 : 3 0 2 1\n" );
             }
         }
 
@@ -102,13 +102,13 @@ SCENARIO("shadow::Mesh manipulation:")
         urban::shadow::Mesh mesh_1(
             "first",
             std::map<size_t, urban::shadow::Point>{{0, urban::shadow::Point(0., 0., 0.)}, {1, urban::shadow::Point(3.5, 1.325, 0.58)}, {3, urban::shadow::Point(6.28, -.2, -.5)}, {2, urban::shadow::Point(4.1, 2.368, 1.2589)}, {4, urban::shadow::Point(-.25, 2.12, .98)}},
-            std::map<size_t, urban::shadow::Face>{{0, urban::shadow::Face(0, 1, 4)}, {1, urban::shadow::Face(1, 3, 2)}, {2, urban::shadow::Face(1, 2, 4)}}
+            std::map<size_t, urban::shadow::Face>{{0, urban::shadow::Face{{0, 1, 4}}}, {1, urban::shadow::Face{{1, 3, 2}}}, {2, urban::shadow::Face{{1, 2, 4}}}}
         );
 
         urban::shadow::Mesh mesh_2(
             "second",
             std::map<size_t, urban::shadow::Point>{{0, urban::shadow::Point(-.549, -8.2, -10.54)}, {1, urban::shadow::Point(3.5, 1.325, 0.58)}, {2, urban::shadow::Point(1.54, -7.98, -5.97)}, {3, urban::shadow::Point(1.014, -6.32, -7.12)}, {4, urban::shadow::Point(6.28, -.2, -.5)}, {5, urban::shadow::Point(0., 0., 0.)}, {6, urban::shadow::Point(-5., -4.95, -9.23)}},
-            std::map<size_t, urban::shadow::Face>{{0, urban::shadow::Face(0, 2, 1)}, {1, urban::shadow::Face(1, 2, 4)}, {2, urban::shadow::Face(4, 2, 3)}, {3, urban::shadow::Face(5, 0, 1)}, {4, urban::shadow::Face(5, 6, 0)}}
+            std::map<size_t, urban::shadow::Face>{{0, urban::shadow::Face{{0, 2, 1}}}, {1, urban::shadow::Face{{1, 2, 4}}}, {2, urban::shadow::Face{{4, 2, 3}}}, {3, urban::shadow::Face{{5, 0, 1}}}, {4, urban::shadow::Face{{5, 6, 0}}}}
         );
 
         WHEN("they are stitched together")
@@ -118,7 +118,7 @@ SCENARIO("shadow::Mesh manipulation:")
             {
                 std::ostringstream auxilary;
                 auxilary << result.at(0);
-                REQUIRE(auxilary.str() == "Name: first_second\nBounding box: -5 6.28 -8.2 2.368 -10.54 1.2589\nPoints: \nPoint 0 : 0 0 0\nPoint 1 : 3.5 1.325 0.58\nPoint 2 : 4.1 2.368 1.2589\nPoint 3 : 6.28 -0.2 -0.5\nPoint 4 : -0.25 2.12 0.98\nPoint 5 : -0.549 -8.2 -10.54\nPoint 6 : 1.54 -7.98 -5.97\nPoint 7 : 1.014 -6.32 -7.12\nPoint 8 : -5 -4.95 -9.23\nFaces: \nFace 0 : 3 0 1 4 \nFace 1 : 3 1 3 2 \nFace 2 : 3 1 2 4 \nFace 3 : 3 5 6 1 \nFace 4 : 3 1 6 3 \nFace 5 : 3 3 6 7 \nFace 6 : 3 0 5 1 \nFace 7 : 3 0 8 5 \n");
+                REQUIRE(auxilary.str() == "Name: first_second\nBounding box: -5 6.28 -8.2 2.368 -10.54 1.2589\nPoints: \nPoint 0 : 0 0 0\nPoint 1 : 3.5 1.325 0.58\nPoint 2 : 4.1 2.368 1.2589\nPoint 3 : 6.28 -0.2 -0.5\nPoint 4 : -0.25 2.12 0.98\nPoint 5 : -0.549 -8.2 -10.54\nPoint 6 : 1.54 -7.98 -5.97\nPoint 7 : 1.014 -6.32 -7.12\nPoint 8 : -5 -4.95 -9.23\nFaces: \nFace 0 : 3 0 1 4\nFace 1 : 3 1 3 2\nFace 2 : 3 1 2 4\nFace 3 : 3 5 6 1\nFace 4 : 3 1 6 3\nFace 5 : 3 3 6 7\nFace 6 : 3 0 5 1\nFace 7 : 3 0 8 5\n");
             }
         }        
     }
