179d317cca519dbf93255edada5ab49a1381bafc
Ethiy <oussama.ennafii@ign.fr>
2017-05-30 14:48:54 +0200

    minor change

--- src/samples/hello_santa.cpp
+++ src/samples/hello_santa.cpp
@@ -40,15 +40,11 @@ int main(int, char **)
         geomview_stream.set_bg_color(CGAL::Color(0, 127, 200));
         std::size_t pigment(1);
         std::size_t all(meshes.size());
-        std::for_each(
-            std::begin(urban_objects),
-            std::end(urban_objects),
-            [&geomview_stream, pigment, all](urban::scene::Brick & obj) mutable
-            {
-                geomview_stream << CGAL::Color(250 * (pigment - 1) / all, 0, 250 * pigment / all) << obj;
-                pigment++;
-            });
-
+        for(auto const& obj : urban_objects)
+        {
+            geomview_stream << CGAL::Color(250 * (pigment - 1) / all, 0, 250 * pigment / all) << obj;
+            pigment++;
+        }
         geomview_stream.look_recenter();
         #endif // CGAL_USE_GEOMVIEW
         std::cout << "Enter any character to stop the program:" << std::endl;
