8477e53f67fc66a8b856c6dbe9c2c41687481276
Ethiy <oussama.ennafii@ign.fr>
2017-06-08 14:54:44 +0200

    read meshes of node

--- src/libs/io/io_3ds.h
+++ src/libs/io/io_3ds.h
@@ -23,13 +23,16 @@ namespace urban
             FileHandler(boost::filesystem::path const& _filepath, std::map<std::string, bool> const& _modes);
             ~FileHandler(void);
 
+            std::vector<shadow::Mesh> read(std::string node_name);
             std::vector<shadow::Mesh> read(void);
-            void write(std::vector<shadow::Mesh> meshes);
 
+            void write(std::vector<shadow::Mesh> meshes);
         private:
             Lib3dsFile* file = NULL;
             boost::filesystem::path filepath;
             std::map<std::string, bool> modes;
+
+            void node_meshes(Lib3dsNode * node, std::vector<shadow::Mesh> & meshes);
         };
     }
 }
