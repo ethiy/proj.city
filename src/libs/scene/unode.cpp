0b2a5afe7303b5f9bded6694a331a86914d675f5
Ethiy <oussama.ennafii@ign.fr>
2017-07-21 15:53:19 +0200

    unode has same api as brick

--- src/libs/scene/unode.cpp
+++ src/libs/scene/unode.cpp
@@ -5,6 +5,13 @@
 #include <CGAL/Polygon_mesh_processing/orientation.h>
 #include <CGAL/Polygon_mesh_processing/stitch_borders.h>
 #include <CGAL/Polygon_mesh_processing/bbox.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+
+#ifdef CGAL_USE_GEOMVIEW
+#include <CGAL/IO/Polyhedron_geomview_ostream.h>
+#endif // CGAL_USE_GEOMVIEW
+
+#include <vector>
 
 
 namespace urban
@@ -129,6 +136,327 @@ namespace urban
         {
             return name;
         }
+
+        unsigned short UNode::get_epsg(void) const noexcept
+        {
+            return epsg_index;
+        }
+
+        shadow::Point UNode::get_reference_point(void) const noexcept
+        {
+            return reference_point;
+        }
+
+        std::size_t UNode::vertices_size(void) const
+        {
+            return surface.size_of_vertices();
+        }
+
+        std::size_t UNode::facets_size(void) const
+        {
+            return surface.size_of_facets();
+        }
+
+        UNode::Facet_iterator UNode::facets_begin(void) noexcept
+        {
+            return surface.facets_begin();
+        }
+        UNode::Facet_iterator UNode::facets_end(void) noexcept
+        {
+            return surface.facets_end();
+        }
+        UNode::Facet_const_iterator UNode::facets_cbegin(void) const noexcept
+        {
+            return surface.facets_begin();
+        }
+        UNode::Facet_const_iterator UNode::facets_cend(void) const noexcept
+        {
+            return surface.facets_end();
+        }
+
+        UNode::Halfedge_iterator UNode::halfedges_begin(void) noexcept
+        {
+            return surface.halfedges_begin();
+        }
+        UNode::Halfedge_iterator UNode::halfedges_end(void) noexcept
+        {
+            return surface.halfedges_end();
+        }
+        UNode::Halfedge_const_iterator UNode::halfedges_cbegin(void) const noexcept
+        {
+            return surface.halfedges_begin();
+        }
+        UNode::Halfedge_const_iterator UNode::halfedges_cend(void) const noexcept
+        {
+            return surface.halfedges_end();
+        }
+
+        UNode::Halfedge_iterator UNode::border_halfedges_begin(void) noexcept
+        {
+            return surface.border_halfedges_begin();
+        }
+
+        UNode::Halfedge_const_iterator UNode::border_halfedges_begin(void) const noexcept
+        {
+            return surface.border_halfedges_begin();
+        }
+
+
+        UNode::Point_iterator UNode::points_begin(void) noexcept
+        {
+            return surface.points_begin();
+        }
+        UNode::Point_iterator UNode::points_end(void) noexcept
+        {
+            return surface.points_end();
+        }
+        UNode::Point_const_iterator UNode::points_cbegin(void) const noexcept
+        {
+            return surface.points_begin();
+        }
+        UNode::Point_const_iterator UNode::points_cend(void) const noexcept
+        {
+            return surface.points_end();
+        }
+
+        UNode::Plane_iterator UNode::planes_begin(void) noexcept
+        {
+            return surface.planes_begin();
+        }
+        UNode::Plane_iterator UNode::planes_end(void) noexcept
+        {
+            return surface.planes_end();
+        }
+        UNode::Plane_const_iterator UNode::planes_cbegin(void) const noexcept
+        {
+            return surface.planes_begin();
+        }
+        UNode::Plane_const_iterator UNode::planes_cend(void) const noexcept
+        {
+            return surface.planes_end();
+        }
+
+        UNode::Halfedge_iterator UNode::prunable(void)
+        {
+            return std::find_if(
+                halfedges_begin(),
+                halfedges_end(),
+                [](Polyhedron::Halfedge const& halfedge)
+                {
+                    bool joinable = !halfedge.is_border_edge();
+                    if(joinable)
+                    {
+                        Point_3 A(halfedge.vertex()->point()),
+                                B(halfedge.next()->vertex()->point()),
+                                C(halfedge.next()->next()->vertex()->point()),
+                                D(halfedge.opposite()->next()->vertex()->point());
+                        joinable = (std::abs(to_double(CGAL::determinant(B - A, C - A, D - A))) < std::numeric_limits<double>::epsilon());
+                    }
+                    return  joinable;
+                }
+            );
+        }
+
+        std::vector<UNode::Halfedge_handle> UNode::combinable(Facet & facet) const
+        {
+            std::vector<UNode::Halfedge_handle> combining_edges;
+            combining_edges.reserve(facet.facet_degree());
+
+            Polyhedron::Halfedge_around_facet_circulator facet_circulator = facet.facet_begin();
+            do
+            {
+                if(!facet_circulator->is_border_edge())
+                {
+                    Point_3 A(facet_circulator->vertex()->point()),
+                            B(facet_circulator->next()->vertex()->point()),
+                            C(facet_circulator->next()->next()->vertex()->point()),
+                            D(facet_circulator->opposite()->next()->vertex()->point());
+                    if(std::abs(to_double(CGAL::determinant(B - A, C - A, D - A))) < std::numeric_limits<double>::epsilon())
+                        combining_edges.push_back(facet_circulator->opposite());
+                }
+            }while(++facet_circulator != facet.facet_begin());
+
+            return combining_edges;
+        }
+
+        std::vector<UNode::Halfedge_handle> UNode::pruning_halfedges(void)
+        {
+            std::vector<UNode::Halfedge_handle> combining_edges;
+            std::vector<UNode::Halfedge_handle> buffer;
+
+            std::for_each(
+                facets_begin(),
+                facets_end(),
+                [&combining_edges, &buffer, this](Facet & facet)
+                {
+                    buffer = combinable(facet);
+                    std::copy_if(
+                        std::begin(buffer),
+                        std::end(buffer),
+                        std::back_inserter(combining_edges),
+                        [&combining_edges](UNode::Halfedge_handle const& h)
+                        {
+                            return std::none_of(
+                                std::begin(combining_edges),
+                                std::end(combining_edges),
+                                [&h](UNode::Halfedge_handle const& present)
+                                {
+                                    return  (present->vertex()->point() == h->vertex()->point() && present->opposite()->vertex()->point() == h->opposite()->vertex()->point())
+                                            ||
+                                            (present->opposite()->vertex()->point() == h->vertex()->point() && present->vertex()->point() == h->opposite()->vertex()->point());
+                                }
+                            );
+                        }
+                    );
+                    buffer.clear();
+                }
+            );
+            return combining_edges;
+        }
+
+        UNode & UNode::join_facet(UNode::Halfedge_handle & h)
+        {
+            surface.join_facet(h);
+            return *this;
+        }
+
+        Point_3 UNode::centroid(UNode::Facet const& facet) const
+        {
+            Polyhedron::Halfedge_around_facet_const_circulator circulator = facet.facet_begin();
+            Vector_3 n = normal(facet);
+
+            Vector_3 centroid = CGAL::NULL_VECTOR;
+            do
+            {
+                centroid =  centroid
+                            +
+                            ((circulator->vertex()->point() - CGAL::ORIGIN) + (circulator->next()->vertex()->point() - CGAL::ORIGIN))
+                                *
+                            to_double(CGAL::cross_product(circulator->vertex()->point() - CGAL::ORIGIN, circulator->next()->vertex()->point() - CGAL::ORIGIN) * n)
+                                /
+                            6;
+            }while(circulator != facet.facet_begin());
+
+            return CGAL::ORIGIN + centroid / area(facet);
+        }
+
+        Vector_3 UNode::normal(UNode::Facet const& facet) const
+        {
+            Polyhedron::Halfedge_around_facet_const_circulator circulator = facet.facet_begin();
+            return CGAL::normal(circulator->vertex()->point(), circulator->next()->vertex()->point(), circulator->next()->next()->vertex()->point());
+        }
+
+        double UNode::area(UNode::Facet const& facet) const
+        {
+            Polyhedron::Halfedge_around_facet_const_circulator circulator = facet.facet_begin();
+            Vector_3 n = normal(facet);
+
+            double area(0);
+            do
+            {
+                area += to_double(CGAL::cross_product(circulator->vertex()->point() - CGAL::ORIGIN, circulator->next()->vertex()->point() - CGAL::ORIGIN) * n/2.);
+            }while(circulator != facet.facet_begin());
+
+            return area;
+        }
+        
+        std::vector<UNode::Facet_const_handle> UNode::facet_adjacents(UNode::Facet const& facet) const
+        {
+            std::vector<UNode::Facet_const_handle> adjacents;
+            adjacents.reserve(facet.facet_degree());
+
+            auto circulator = facet.facet_begin();
+            do
+            {
+                auto buff = circulator->opposite()->facet();
+                if(!circulator->is_border() && buff != NULL)
+                    adjacents.push_back(buff);
+            }while(++circulator != facet.facet_begin());
+            return adjacents;
+        }
+
+        std::vector<bool> UNode::facet_adjacency_matrix(void) const
+        {
+            std::vector<bool> adjacency(facets_size() * facets_size(), false);
+            return facet_adjacency_matrix(adjacency, 0);
+        }
+
+        std::vector<bool> & UNode::facet_adjacency_matrix(std::vector<bool> & matrix, std::size_t offset) const
+        {
+            std::size_t n = static_cast<std::size_t>(std::floor(std::sqrt(matrix.size())));
+            std::size_t size = facets_size();
+
+            if(n * n != matrix.size())
+                throw std::logic_error("The adjacency matrix must be square!");
+            if(n < offset + facets_size())
+                throw std::underflow_error("The matrix cannot hold the whole brick!");
+
+            std::vector<UNode::Facet_const_handle> facets(size);
+            std::transform(
+                facets_cbegin(),
+                facets_cend(),
+                std::begin(facets),
+                [](UNode::Facet const& facet)
+                {
+                    return &facet;
+                }
+            );
+
+            for(std::size_t diag(offset); diag != size + offset; ++diag)
+                matrix.at(diag * size + diag) = true;
+
+            std::vector<UNode::Facet_const_handle> line_adjacents;
+            for(std::size_t line(0); line != size; ++line)
+            {
+                line_adjacents = facet_adjacents(*facets.at(line));
+
+                for(auto adjacent : line_adjacents)
+                {
+                    auto placeholder = std::find(std::begin(facets), std::end(facets), adjacent);
+                    if(placeholder != std::end(facets))
+                    {
+                        long index = std::distance(std::begin(facets), placeholder) + offset;
+                        matrix.at((line + offset) * size + index) = true;
+                    }
+                }
+
+                line_adjacents.clear();
+            }   
+            return matrix;
+        }
+
+        std::ostream & operator<<(std::ostream &os, UNode const& unode)
+        {
+            os  << "# Name: " << unode.name << std::endl
+                << unode.surface;
+            return os;
+        }
+
+        io::Adjacency_stream & operator<<(io::Adjacency_stream & as, UNode const& unode)
+        {
+            std::for_each(
+                unode.facets_cbegin(),
+                unode.facets_cend(),
+                [&as, &unode](UNode::Facet const& facet)
+                {
+                    as << facet.facet_degree() << " " << unode.area(facet) << " " << unode.centroid(facet) << unode.normal(facet) << std::endl;
+                }
+            );
+
+            std::vector<bool> matrix = unode.facet_adjacency_matrix();
+
+            as << matrix << std::endl;
+
+            return as;
+        }
+
+        #ifdef CGAL_USE_GEOMVIEW
+        CGAL::Geomview_stream & operator<<(CGAL::Geomview_stream &gs, UNode const& unode)
+        {
+            gs << unode.surface;
+            return gs;
+        }
+        #endif // CGAL_USE_GEOMVIEW
 
         void swap(UNode & lhs, UNode & rhs)
         {
--- src/libs/scene/unode.h
+++ src/libs/scene/unode.h
@@ -6,7 +6,14 @@
 
 #include "../io/io_3ds.h"
 
+#include "../io/Adjacency_stream/adjacency_stream.h"
+
+#ifdef CGAL_USE_GEOMVIEW
+#include <CGAL/IO/Geomview_stream.h>
+#endif // CGAL_USE_GEOMVIEW
+
 #include <string>
+#include <ostream>
 
 namespace urban
 {
@@ -32,6 +39,216 @@ namespace urban
             Bbox_3 bbox(void) const;
 
             std::string get_name(void) const;
+            /**
+            * Access urban node epsg.
+            * @return urban node epsg
+            */
+            unsigned short get_epsg(void) const noexcept;
+            /**
+            * Access urban node reference point.
+            * @return urban node reference point
+            */
+            shadow::Point get_reference_point(void) const noexcept;
+            /**
+            * Get the number of vertices
+            * @return number of vertices
+            */
+            std::size_t vertices_size(void) const;
+            /**
+            * Get the number of facets
+            * @return number of facets
+            */
+            std::size_t facets_size(void) const;
+
+            /** Halfedge handle */
+            using Halfedge_handle = Polyhedron::Halfedge_handle;
+            /** Halfedge constant handle */
+            using Halfedge_const_handle = Polyhedron::Halfedge_const_handle;
+            /** Halfedge iterator*/
+            using Halfedge_iterator = Polyhedron::Halfedge_iterator;
+            /** Halfedge constant iterator*/
+            using Halfedge_const_iterator = Polyhedron::Halfedge_const_iterator;
+
+            /** Facet */
+            using Facet = Polyhedron::Facet;
+            /** Facet handle */
+            using Facet_handle = Polyhedron::Facet_handle;
+            /** Facet constant handle */
+            using Facet_const_handle = Polyhedron::Facet_const_handle;
+            /** Facet iterator */
+            using Facet_iterator = Polyhedron::Facet_iterator;
+            /** Facet constant iterator */
+            using Facet_const_iterator = Polyhedron::Facet_const_iterator;
+
+            /** Points iterator */
+            using Point_iterator = Polyhedron::Point_iterator;
+            /** Points constant iterator */
+            using Point_const_iterator = Polyhedron::Point_const_iterator;
+
+            /** Plane iterator */
+            using Plane_iterator = Polyhedron::Plane_iterator;
+            /** Plane constant iterator */
+            using Plane_const_iterator = Polyhedron::Plane_const_iterator;
+
+            /**
+            * Returns facets begin iterator
+            * @return facets begin iterator
+            */
+            Facet_iterator facets_begin(void) noexcept;
+            /**
+            * Returns facets end iterator
+            * @return facets end iterator
+            */
+            Facet_iterator facets_end(void) noexcept;
+            /**
+            * Returns facets constant begin iterator
+            * @return facets constant begin iterator
+            */
+            Facet_const_iterator facets_cbegin(void) const noexcept;
+            /**
+            * Returns facets constant end iterator
+            * @return facets constant end iterator
+            */
+            Facet_const_iterator facets_cend(void) const noexcept;
+
+            /**
+            * Returns halfedges begin iterator
+            * @return halfedges begin iterator
+            */
+            Halfedge_iterator halfedges_begin(void) noexcept;
+            /**
+            * Returns halfedges end iterator
+            * @return halfedges end iterator
+            */
+            Halfedge_iterator halfedges_end(void) noexcept;
+            /**
+            * Returns halfedges constant begin iterator
+            * @return halfedges constant begin iterator
+            */
+            Halfedge_const_iterator halfedges_cbegin(void) const noexcept;
+            /**
+            * Returns halfedges constant end iterator
+            * @return halfedges constant end iterator
+            */
+            Halfedge_const_iterator halfedges_cend(void) const noexcept;
+            /**
+            * Returns border halfedges begin iterator
+            * @return border halfedges begin iterator
+            */
+            Halfedge_iterator border_halfedges_begin(void) noexcept;
+            /**
+            * Returns border halfedges constant begin iterator
+            * @return border halfedges constant begin iterator
+            */
+            Halfedge_const_iterator border_halfedges_begin(void) const noexcept;
+
+            /**
+            * Returns points begin iterator
+            * @return points begin iterator
+            */
+            Point_iterator points_begin(void) noexcept;
+            /**
+            * Returns points end iterator
+            * @return points end iterator
+            */
+            Point_iterator points_end(void) noexcept;
+            /**
+            * Returns points constant begin iterator
+            * @return points constant begin iterator
+            */
+            Point_const_iterator points_cbegin(void) const noexcept;
+            /**
+            * Returns points constant end iterator
+            * @return points constant end iterator
+            */
+            Point_const_iterator points_cend(void) const noexcept;
+
+            /**
+            * Returns planes begin iterator
+            * @return planes begin iterator
+            */
+            Plane_iterator planes_begin(void) noexcept;
+            /**
+            * Returns planes end iterator
+            * @return planes end iterator
+            */
+            Plane_iterator planes_end(void) noexcept;
+            /**
+            * Returns planes constant begin iterator
+            * @return planes constant begin iterator
+            */
+            Plane_const_iterator planes_cbegin(void) const noexcept;
+            /**
+            * Returns planes constant end iterator
+            * @return planes constant end iterator
+            */
+            Plane_const_iterator planes_cend(void) const noexcept;
+
+            /**
+            * Compute the centroid of a urban node facet
+            * @param facet a urban node facet
+            * @return centroid of the facet
+            */
+            Point_3 centroid(const UNode::Facet & facet) const;
+            /**
+            * Compute the normal of a urban node facet
+            * @param facet a urban node facet
+            * @return the normal of the facet
+            */
+            Vector_3 normal(const UNode::Facet & facet) const;
+            /**
+            * Compute the area of a urban node facet
+            * @param facet a urban node facet
+            * @return area of the facet
+            */
+            double area(const UNode::Facet & facet) const;
+
+            /** 
+            * Finds a joinable halfedge.
+            * @return a halfedge handle of joinable facets
+            */
+            UNode::Halfedge_iterator prunable(void);
+
+            /** 
+            * Finds all joinable halfedges.
+            * @param facet a urban node facet
+            * @return a vector of pruning halfedge handles for the facet
+            */
+            std::vector<UNode::Halfedge_handle> combinable(Facet & facet) const;
+
+            /** 
+            * Clusters all facets into prunable facet bags.
+            * @return a vector of all pruning halfedges
+            */
+            std::vector<UNode::Halfedge_handle> pruning_halfedges(void);
+
+            /** 
+            * Join prunable halfedge
+            * Wraps CGAL join_facet() for urban node.
+            * @param h halfedge handle to join its incidents facets
+            * @return `this` urban node modified
+            */
+            UNode & join_facet(Halfedge_handle & h);
+
+            /** 
+            * Get facets adjacent to a facet
+            * @param facet a urban node facet
+            * @return handle to facets adjacent to facet
+            */
+            std::vector<UNode::Facet_const_handle> facet_adjacents(UNode::Facet const& facet) const;
+
+            /** 
+            * Get facet adjacency matrix (dual matrix)
+            * @return the facet adjacency matrix
+            */
+            std::vector<bool> facet_adjacency_matrix(void) const;
+            /** 
+            * Write facet adjacency matrix to building matrix
+            * @param matrix the matrix to write on
+            * @param offset the index of the block where to write
+            * @return the matrix
+            */
+            std::vector<bool> & facet_adjacency_matrix(std::vector<bool> & matrix, std::size_t offset) const;
         private:
             /** Node name */
             std::string name;
@@ -43,6 +260,30 @@ namespace urban
             Polyhedron surface;
             /** Bounding box*/
             Bbox_3 bounding_box;
+
+            /**
+            * Outstreaming urban node in OFF format
+            * @param os the output stream
+            * @param brick the brick to stream
+            * @return the output stream
+            */
+            friend std::ostream& operator<<(std::ostream & os, UNode const& unode);
+            /**
+            * Outstreaming the dual adjacency graph
+            * @param as the output stream
+            * @param unode the unode to stream
+            * @return the output stream
+            */
+            friend io::Adjacency_stream& operator<<(io::Adjacency_stream & as, UNode const& unode);
+            #ifdef CGAL_USE_GEOMVIEW
+            /**
+            * Outstreaming urban node to GeomView
+            * @param gs the output stream
+            * @param unode the unode to stream
+            * @return the output stream
+            */
+            friend CGAL::Geomview_stream& operator<<(CGAL::Geomview_stream & gs, UNode const& unode);
+            #endif // CGAL_USE_GEOMVIEW
         };
 
         void swap(UNode & lhs, UNode & rhs);
