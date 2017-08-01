3580f72d08b852df01bc061e375cef8b29e01eef
Ethiy <oussama.ennafii@ign.fr>
2017-08-01 17:46:41 +0200

    adopting using instead of typedef

--- src/include/geometry_definitions.h
+++ src/include/geometry_definitions.h
@@ -14,29 +14,29 @@
 
 namespace urban
 {
-    typedef CGAL::Simple_cartesian<double> InexactKernel;
-    typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
-    typedef CGAL::Cartesian_converter<Kernel, InexactKernel> ExactToInexact;
-    typedef CGAL::Cartesian_converter<InexactKernel, Kernel> InexactToExact;
+    using InexactKernel = CGAL::Simple_cartesian<double>;
+    using Kernel = CGAL::Exact_predicates_exact_constructions_kernel;
+    using ExactToInexact = CGAL::Cartesian_converter<Kernel, InexactKernel>;
+    using InexactToExact = CGAL::Cartesian_converter<InexactKernel, Kernel>;
 
-    typedef Kernel::Point_3 Point_3;
-    typedef Kernel::Vector_3 Vector_3;
-    typedef Kernel::Plane_3 Plane_3;
-    typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
-    typedef CGAL::Color Color;
-    typedef CGAL::Aff_transformation_3<Kernel> Affine_transformation_3;
-    typedef CGAL::Bbox_3 Bbox_3;
+    using Point_3 = Kernel::Point_3;
+    using Vector_3 = Kernel::Vector_3;
+    using Plane_3 = Kernel::Plane_3;
+    using Polyhedron = CGAL::Polyhedron_3<Kernel>;
+    using Color = CGAL::Color;
+    using Affine_transformation_3 = CGAL::Aff_transformation_3<Kernel>;
+    using Bbox_3 = CGAL::Bbox_3;
 
-    typedef CGAL::Polygon_2<Kernel> Polygon;
-    typedef CGAL::Polygon_2<InexactKernel> InexactPolygon;
-    typedef CGAL::Polygon_with_holes_2<Kernel> Polygon_with_holes;
-    typedef CGAL::Polygon_with_holes_2<InexactKernel> InexactPolygon_with_holes;
-    typedef CGAL::Polygon_set_2<Kernel> Polygon_set;
-    typedef Kernel::Point_2 Point_2;
-    typedef InexactKernel::Point_2 InexactPoint_2;
-    typedef Kernel::Vector_2 Vector_2;
-    typedef InexactKernel::Vector_2 InexactVector_2;
-    typedef Kernel::Intersect_2 Intersect_2;
-    typedef CGAL::Aff_transformation_2<Kernel> Affine_transformation_2;
-    typedef CGAL::Bbox_2 Bbox_2;
+    using Polygon = CGAL::Polygon_2<Kernel>;
+    using InexactPolygon = CGAL::Polygon_2<InexactKernel>;
+    using Polygon_with_holes = CGAL::Polygon_with_holes_2<Kernel>;
+    using InexactPolygon_with_holes = CGAL::Polygon_with_holes_2<InexactKernel>;
+    using Polygon_set = CGAL::Polygon_set_2<Kernel>;
+    using Point_2 = Kernel::Point_2;
+    using InexactPoint_2 = InexactKernel::Point_2;
+    using Vector_2 = Kernel::Vector_2;
+    using InexactVector_2 = InexactKernel::Vector_2;
+    using Intersect_2 = Kernel::Intersect_2;
+    using Affine_transformation_2 = CGAL::Aff_transformation_2<Kernel>;
+    using Bbox_2 = CGAL::Bbox_2;
 }
