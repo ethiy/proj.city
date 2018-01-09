19089af88fcd215d01873fdccb8930e75a64cf70
Oussama ENNAFII <oussama.ennafii@polytechnique.org>
2018-01-09 17:23:39 +0000

    minor change

--- src/include/algorithms/algorithms.h
+++ src/include/algorithms/algorithms.h
@@ -2,6 +2,7 @@
 
 #include <algorithms/unode_algorithms.h>
 #include <algorithms/scene_algorithms.h>
+#include <algorithms/io_algorithms.h>
 
 #include <algorithms/util_algorithms.h>
 #include <algorithms/test_utils.h>
