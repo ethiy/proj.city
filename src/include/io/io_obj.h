af551b32a97dff74c445dd1cd8efc713668cddf4
Ethiy <oussama.ennafii@ign.fr>
2018-05-09 17:40:14 +0200

    obj scene handlr

--- src/include/io/io_obj.h
+++ src/include/io/io_obj.h
@@ -5,6 +5,8 @@
 #include <shadow/mesh.h>
 #include <scene/scene.h>
 
+#include <boost/optional.hpp>
+
 
 namespace city
 {
@@ -14,8 +16,8 @@ namespace city
         {
         public:
             WaveObjHandler(boost::filesystem::path const& _filepath, std::map<std::string, bool> const& _modes);
-            WaveObjHandler(boost::filesystem::path const& _filepath, std::vector<shadow::Mesh> const& _meshes, std::map<std::string, bool> const& _modes = std::map<std::string, bool>{{"write", true}});
-            WaveObjHandler(boost::filesystem::path const& _filepath, scene::Scene const& scene, std::map<std::string, bool> const& _modes = std::map<std::string, bool>{{"write", true}});
+            WaveObjHandler(boost::filesystem::path const& _filepath, std::vector<shadow::Mesh> const& _meshes);
+            WaveObjHandler(boost::filesystem::path const& _filepath, std::vector<scene::UNode> const& unodes);
             ~WaveObjHandler(void);
 
             std::vector<shadow::Mesh> const& data(void) const noexcept;
@@ -23,6 +25,7 @@ namespace city
             WaveObjHandler& read(void);
 
             shadow::Mesh exclude_mesh(std::string const& excluded);
+            boost::optional<shadow::Mesh> mesh(std::string const& id);
             void add_mesh(shadow::Mesh const& mesh);
 
             void write(void);
@@ -30,6 +33,26 @@ namespace city
             scene::Scene get_scene(void);
         private:
             std::vector<shadow::Mesh> meshes;
+        };
+
+        class WaveObjSceneHandler: protected WaveObjHandler
+        {
+        public:
+            WaveObjSceneHandler(boost::filesystem::path const& _filepath, std::map<std::string, bool> const& _modes);
+            WaveObjSceneHandler(boost::filesystem::path const& _filepath, scene::Scene const& scene);
+            ~WaveObjSceneHandler();
+
+            scene::Scene read(bool const using_xml = true);
+
+            void write(bool const with_xml = true);
+        private:
+            boost::filesystem::path scene_tree_path;
+
+            shadow::Point pivot;
+            shadow::Bbox bbox;
+            unsigned short epsg_index;
+            std::vector<std::string> building_ids;
+            std::string terrain_id;
         };
     }
 }
--- src/lib/io/io_obj.cpp
+++ src/lib/io/io_obj.cpp
@@ -1,6 +1,7 @@
 #include <io/io_obj.h>
 
 #include <io/Obj_stream/obj_stream.h>
+#include <io/io_scene_tree.h>
 
 #include <fstream>
 #include <sstream>
@@ -12,22 +13,21 @@ namespace city
         WaveObjHandler::WaveObjHandler(boost::filesystem::path const& _filepath, std::map<std::string, bool> const& _modes)
             : FileHandler(_filepath, _modes)
         {}
-        WaveObjHandler::WaveObjHandler(boost::filesystem::path const& _filepath, std::vector<shadow::Mesh> const& _meshes, std::map<std::string, bool> const& _modes)
-            : FileHandler(_filepath, _modes), meshes(_meshes)
+        WaveObjHandler::WaveObjHandler(boost::filesystem::path const& _filepath, std::vector<shadow::Mesh> const& _meshes)
+            : FileHandler(_filepath, std::map<std::string, bool>{{"write", true}}), meshes(_meshes)
         {}
-        WaveObjHandler::WaveObjHandler(boost::filesystem::path const& _filepath, scene::Scene const& scene, std::map<std::string, bool> const& _modes)
-            : WaveObjHandler(_filepath, std::vector<shadow::Mesh>(scene.size() + 1), _modes)
+        WaveObjHandler::WaveObjHandler(boost::filesystem::path const& _filepath, std::vector<scene::UNode> const& unodes)
+            : FileHandler(_filepath, std::map<std::string, bool>{{"write", true}}), meshes(unodes.size())
         {
             std::transform(
-                std::begin(scene),
-                std::end(scene),
+                std::begin(unodes),
+                std::end(unodes),
                 std::begin(meshes),
                 [](scene::UNode const& unode)
                 {
                     return shadow::Mesh(unode);
                 }
             );
-            meshes.back() = shadow::Mesh(scene.get_terrain());
         }
         WaveObjHandler::~WaveObjHandler(void) {}
 
@@ -102,18 +102,97 @@ namespace city
                 shadow::Mesh()
             ).set_name(excluded);
         }
+        boost::optional<shadow::Mesh> WaveObjHandler::mesh(std::string const& id)
+        {
+            auto found = std::find_if(
+                std::begin(meshes),
+                std::end(meshes),
+                [id](shadow::Mesh const & m)
+                {
+                    return m.get_name() == id;
+                }
+            );
+            if(found == std::end(meshes))
+                return boost::none;
+            else
+                return *found;
+        }
         void WaveObjHandler::add_mesh(shadow::Mesh const& mesh)
         {
             meshes.push_back(mesh);
         }
 
-        scene::Scene WaveObjHandler::get_scene(void)
+
+        WaveObjSceneHandler::WaveObjSceneHandler(boost::filesystem::path const& _filepath, std::map<std::string, bool> const& _modes)
+            : WaveObjHandler(_filepath, _modes)
+        {}
+        WaveObjSceneHandler::WaveObjSceneHandler(boost::filesystem::path const& _filepath, scene::Scene const& scene)
+            : WaveObjHandler(_filepath, scene.all_nodes()),
+              pivot(scene.get_pivot()),
+              bbox(scene.bbox()),
+              building_ids(scene.identifiers()),
+              terrain_id(scene.get_terrain().get_name())
+        {}
+        WaveObjSceneHandler::~WaveObjSceneHandler()
+        {}
+
+        scene::Scene WaveObjSceneHandler::read(bool const using_xml)
         {
-            auto terrain = read().exclude_mesh("terrain");
-            return scene::Scene(
-                meshes,
-                terrain
-            );
+            if(!boost::filesystem::is_regular_file(scene_tree_path))
+            {
+                if(using_xml)
+                    throw std::logic_error("Cannot extract from a file: it does not exist!");
+                else
+                {
+                    auto terrain = WaveObjHandler::read().exclude_mesh("terrain");
+                    return scene::Scene(
+                        data(),
+                        terrain
+                    );
+                }
+            }
+            else
+            {
+                SceneTreeHandler scene_tree(scene_tree_path, modes);
+                if(using_xml)
+                {
+                    auto _building_ids = scene_tree.building_ids();
+                    std::vector<shadow::Mesh> building_meshes(building_ids.size());
+                    std::transform(
+                        std::begin(building_ids),
+                        std::end(building_ids),
+                        std::begin(building_meshes),
+                        [this](std::string const& building_id)
+                        {
+                            return mesh(building_id).value_or(shadow::Mesh());
+                        }
+                    );
+                    return scene::Scene(
+                        building_meshes,
+                        mesh(terrain_id).value_or(shadow::Mesh()),
+                        scene_tree.pivot(),
+                        scene_tree.epsg_index()
+                    );
+                }
+                else
+                {
+                    auto terrain = WaveObjHandler::read().exclude_mesh("terrain");
+                    return scene::Scene(
+                        data(),
+                        terrain,
+                        scene_tree.pivot(),
+                        scene_tree.epsg_index()
+                    );
+
+                }
+            }
+        }
+
+        void WaveObjSceneHandler::write(bool const with_xml)
+        {
+            WaveObjHandler::write();
+            if(with_xml)
+                SceneTreeHandler(scene_tree_path, modes).write(pivot, bbox, epsg_index, building_ids, terrain_id);
         }
     }
 }
